610abe1529e6812a4f1cee8dc5fe18e9c550d15d
SabrinaSavaglia <sabrinasavaglia18@gmail.com>
2025-08-19 15:49:19 -0300

    .

--- Obligatorio React/Obligatorio React/src/componentes/dashboard/TituloBuscarUsuariosPorPais.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TituloBuscarUsuariosPorPais = () => {
-  return (
-    <div className="card-header">Usuarios Por Pais</div>
-  )
-}
-
-export default TituloBuscarUsuariosPorPais
--- Obligatorio React/Obligatorio React/src/componentes/dashboard/TituloNuevaEvaluacion.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TituloNuevaEvaluacion = () => {
-  return <div className="card-header">Nueva Evaluación</div>;
-};
-
-export default TituloNuevaEvaluacion;
